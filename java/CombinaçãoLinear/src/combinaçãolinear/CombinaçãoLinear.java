7876a73c599202ec99b48ef1f1252d3cc17f829c
DavidRoseno <gfdavid13@gmail.com>
2018-12-24 17:11:24 -0200

    declarando variáveis e preenchendo vetores

--- java/CombinaçãoLinear/src/combinaçãolinear/CombinaçãoLinear.java
+++ java/CombinaçãoLinear/src/combinaçãolinear/CombinaçãoLinear.java
@@ -17,10 +17,7 @@ public class CombinaçãoLinear {
         int a, b; //Coeficientes;
         int resA[] = new int[3]; //Recebe o resultado da conta entre o coeficiente e cada respectiva posição do vetor<a.v1>
         int resB[] = new int[3]; //Recebe o resultado da conta entre o coeficiente e cada respectiva posição do vetor<b.v2>
-        int comb[] = new int[3]; 
-        
-        System.out.println("-----------Combinação Linear-----------\n");
-        System.out.println("Digite os valores dos vetores abaixo:");
+        int comb[] = new int[3]; //Armazena o a soma dos vetores de mesma posição( resA[] + resB[] )
         System.out.println("Digite o vetor resultante: ");
         for(int i=0; i<3; i++){
             W[i] = in.nextInt();
@@ -33,15 +30,6 @@ public class CombinaçãoLinear {
         for(int i=0; i<3; i++){
             v2[i] = in.nextInt();
         }
-        System.out.println("Digite o valor do primeiro coeficiente <a>: ");
-        a = in.nextInt();
-        System.out.println("Digite o valor do segundo coeficiente <b>: ");
-        b = in.nextInt();
-        
-        System.out.println("\nW = a.v1 + b.v2");
-        System.out.println("("+W[0]+","+W[1]+","+W[2]+")" + " = " + a + "." + "("+v1[0]+","+v1[1]+","+v1[2]+")" + " + " +
-                           b + "." + "("+v2[0]+","+v2[1]+","+v2[2]+")");
-        
         
     }
     
